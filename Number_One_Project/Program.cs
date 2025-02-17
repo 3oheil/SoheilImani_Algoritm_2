0509caf993bcd87c81800698cf9b23cc6af066d7
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-17 11:46:00 -0800

    Add a new Console app Project for find Prime number

---
+++ Number_One_Project/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Number_One_Project
+{
+    public class Program
+    {
+        static bool IsPrime(int number)
+        {
+            if (number < 2) return false;
+
+            for (int i = 2; i < Math.Sqrt(number); i++)
+            {
+                if (number %i == 0) return false;
+            }
+            return true;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Please Give me a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (IsPrime(number))
+                Console.WriteLine($"{number} is a Prime number...");
+            else
+                Console.WriteLine($"{number} is not a Prime number...");
+        }
+    }
+}
