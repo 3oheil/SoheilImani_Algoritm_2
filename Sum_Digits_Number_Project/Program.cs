00ba2d5254f27f400506460644acd61c8fad52c5
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-17 11:36:37 -0800

    Add consoleApp Project for sum of Degits a number

---
+++ Sum_Digits_Number_Project/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Sum_Digits_Number_Project
+{
+    public class Program
+    {
+        static int SumOfDigit(int number)
+        {
+            int sum = 0;
+
+            while (number > 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            return sum;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Give me a number please: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine($"sum of Digit is {SumOfDigit(num)}");
+        }
+    }
+}
