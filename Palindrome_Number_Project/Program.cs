88343b74f2740d8de6a0dbad59a2f5c1e6ead309
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-17 11:54:29 -0800

    Add new Project to Check the palindrome number

---
+++ Palindrome_Number_Project/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Palindrome_Number_Project
+{
+    public class Program
+    {
+        static bool IsPalindrome(int number)
+        {
+            if (number < 0) return false;
+            int orginal= number;
+            int reversed = 0;
+
+            while (number > 0)
+            {
+                int digit = number% 10;
+                reversed = reversed*10+digit;
+                number/=10;
+            }
+
+            return orginal == reversed;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("enter a number please: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if(IsPalindrome(num))
+                Console.WriteLine($"{num} is palindrome");
+            else
+            {
+                Console.WriteLine($"{num} is not palindrome");
+            }
+        }
+    }
+}
