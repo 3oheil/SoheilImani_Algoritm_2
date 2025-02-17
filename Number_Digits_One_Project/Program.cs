4eca19cb82dcc2e02be26a7850993e653219fa73
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-16 23:09:22 -0800

    a first Console app Project for The number of digits is one

---
+++ Number_Digits_One_Project/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Number_Digits_One_Project
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give me a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            List<int> list = new List<int>();   
+            while (number > 0)
+            {
+                list.Add(number % 10);
+                number = number / 10;
+            }
+            //list.Sort();
+
+            Console.WriteLine(list.Count());
+            Console.ReadKey();
+
+        }
+    }
+}
