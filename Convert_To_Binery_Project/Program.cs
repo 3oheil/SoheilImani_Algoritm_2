629f30f8e007ecfeb26884936e073749ac0e5722
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-18 02:18:43 -0800

    this project converted a number to binery

---
+++ Convert_To_Binery_Project/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Convert_To_Binery_Project
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Give me a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            string binary = Convert.ToString(number, 2);
+
+            Console.WriteLine($"Binery representation: {binary}");
+        }
+    }
+}
