ec6c5f7a3ecc96658916979e02955362f252f8c6
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-18 03:14:13 -0800

    This project is going to show us if there is a repeated number in an array or not?

---
+++ Repeated_Number_Project/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Repeated_Number_Project
+{
+    public class Program
+    {
+        static bool HasDuplicate(int[] arr)
+        {
+            HashSet<int> seen = new HashSet<int>();
+
+            foreach (int num in arr)
+            {
+
+                if (!seen.Add(num))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+
+            static void Main(string[] args)
+            {
+                Console.Write("enter numbers to array and sorted by space: ");
+                string input = Console.ReadLine();
+
+                int[] numbers = Array.ConvertAll(input.Split(' '), int.Parse);
+
+                if (HasDuplicate(numbers))
+                {
+                    Console.WriteLine("There is a duplicate number");
+                }
+                else
+                {
+                    Console.WriteLine("There is no a duplicate number");
+                }
+            }
+        }
+    }
+}
