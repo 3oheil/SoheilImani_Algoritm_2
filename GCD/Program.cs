6af6aa991926c3af5f2544845b85bfd5f749708a
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-17 22:18:41 -0800

    this project to find GCD in two number

---
+++ GCD/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace GCD
+{
+    public class Program
+    {
+        static int GCD(int firstNumber, int secondNumber)
+        {
+            firstNumber = Math.Abs(firstNumber);
+            secondNumber = Math.Abs(secondNumber);
+
+            while (secondNumber != 0)
+            {
+                int temp = secondNumber;
+                secondNumber = firstNumber % secondNumber;
+                firstNumber = temp;
+            }
+            return firstNumber;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Please Give me first number: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please Give me second Number: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"GCD of {firstNumber} and {secondNumber} is ({GCD(firstNumber, secondNumber)})");
+        }
+    }
+}
