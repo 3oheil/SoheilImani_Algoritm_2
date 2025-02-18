4dcfd6774d1939488fdf7f3b65108d7fdc8ecce7
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-18 07:18:30 -0800

    This project is going to take a string from the user and predict all possible situations for moving the characters.

---
+++ Permutation_Project/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Permutation_Project
+{
+    public class Program
+    {
+        static void GeneratePermutations(string str)
+        {
+            var result = GetPermutations(str.ToCharArray(), 0, str.Length - 1);
+            foreach (var item in result)
+            {
+                Console.WriteLine(new string(item));
+            }
+        }
+
+        static char[][] GetPermutations(char[] array, int l, int r)
+        {
+            var result = new List<char[]>();
+
+            if (l == r)
+            {
+                result.Add((char[])array.Clone());
+            }
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    Swap(array, l, i);
+                    result.AddRange(GetPermutations(array, l + 1, r));
+                    Swap(array, l, i);
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        static void Swap(char[] array, int i, int j)
+        {
+            char temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+        static void Main()
+        {
+            Console.Write("Enter a string: ");
+            string? input = Console.ReadLine();
+
+            GeneratePermutations(input);
+        }
+    }
+}
+
