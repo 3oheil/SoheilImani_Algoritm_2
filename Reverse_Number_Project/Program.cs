af0a4ee58e1a4d45f8d5a5cd9f63edfa840dcb42
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-18 01:59:28 -0800

    this console app for revered a number and print for user.

---
+++ Reverse_Number_Project/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Reverse_Number_Project
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int reverse = 0;
+            while (number != 0)
+            {
+                int digit = number % 10;
+                reverse = reverse * 10 + digit;
+                number /= 10;
+            }
+
+            Console.WriteLine($"reversed number is {reverse}");
+        }
+    }
+}
